9a822c1277b6afbed65ef15f11593243ae2d2986
ThanhPhong <phongnt@applancer.net>
2018-07-14 16:27:39 +0700

    abc

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import reducer from './reducers'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import './App.css'
+
+// Import Views
+import Category from './views/Category'
+import CreatePost from './views/CreatePost'
+import Home from './views/Home'
+import PostDetail from './views/PostDetail'
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
+        </header>
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
